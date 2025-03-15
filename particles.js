082a8fda0e06c2668a3fc1140ff1209cb3ee0b9e
Pavan Gaonkar <pavangaonkar109@gmail.com>
2025-03-15 13:16:14 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -11,8 +11,8 @@ for (let i = 0; i < 50; i++) {
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
         size: Math.random() * 3,
-        speedX: (Math.random() - 0.5) * 2,
-        speedY: (Math.random() - 0.5) * 2
+        speedX: (Math.random() - 0.5) * 1.5,
+        speedY: (Math.random() - 0.5) * 1.5
     });
 }
 
